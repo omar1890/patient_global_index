b50262ff7be12a485f6f1b5cd20020b6645b55b1
Abdallah Gamal <abdoolly@gmail.com>
2023-01-31 04:25:31 +0200

    seeder changes

--- database/seeders/PermissionRoleTableSeeder.php
+++ database/seeders/PermissionRoleTableSeeder.php
@@ -16,5 +16,46 @@ class PermissionRoleTableSeeder extends Seeder
             return substr($permission->title, 0, 5) != 'user_' && substr($permission->title, 0, 5) != 'role_' && substr($permission->title, 0, 11) != 'permission_';
         });
         Role::findOrFail(2)->permissions()->sync($user_permissions);
+        $patient_permissions = Permission::whereIn('title', [
+            'profile_password_edit',
+            'patient_edit',
+            'patient_show',
+            'patient_access',
+            'patient_record_access',
+
+            'surgery_create',
+            'surgery_edit',
+            'surgery_show',
+            'surgery_delete',
+            'surgery_access',
+
+            'disease_create',
+            'disease_edit',
+            'disease_show',
+            'disease_delete',
+            'disease_access',
+
+            'vaccine_create',
+            'vaccine_edit',
+            'vaccine_show',
+            'vaccine_delete',
+            'vaccine_access',
+
+            'allergy_create',
+            'allergy_edit',
+            'allergy_show',
+            'allergy_delete',
+            'allergy_access',
+
+            'patient_medicine_create',
+            'patient_medicine_edit',
+            'patient_medicine_show',
+            'patient_medicine_delete',
+            'patient_medicine_access',
+
+            'patient_visit_show',
+            'patient_visit_access',
+        ])->get();
+        Role::findOrFail(3)->permissions()->sync($patient_permissions);
     }
 }
